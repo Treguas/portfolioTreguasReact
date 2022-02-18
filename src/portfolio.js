946daeee409759f1338306c878dc8ba778f8ca4a
Vagner Treguas <vagner.treguas@gmail.com>
2022-02-17 22:32:19 -0300

    section Projects

--- src/portfolio.js
+++ src/portfolio.js
@@ -28,33 +28,6 @@ export const about = {
   },
 }
 
-export const projects = [
-  // {
-  //   name: 'Project 1',
-  //   description:
-  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-  //   stack: ['SASS', 'TypeScript', 'React'],
-  //   sourceCode: 'https://github.com',
-  //   livePreview: 'https://github.com',
-  // },
-  // {
-  //   name: 'Project 2',
-  //   description:
-  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-  //   stack: ['SASS', 'TypeScript', 'React'],
-  //   sourceCode: 'https://github.com',
-  //   livePreview: 'https://github.com',
-  // },
-  // {
-  //   name: 'Project 3',
-  //   description:
-  //     'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-  //   stack: ['SASS', 'TypeScript', 'React'],
-  //   sourceCode: 'https://github.com',
-  //   livePreview: 'https://github.com',
-  // },
-]
-
 export const skills = [
   'HTML',
   'CSS',
